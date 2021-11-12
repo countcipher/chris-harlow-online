563b649399c1a752fec5e1175ec0ec37787b2201
Count Cipher <chrisharlow366@yahoo.com>
2021-11-12 00:05:08 -0500

    first commit, again

---
+++ app.js
@@ -0,0 +1,11 @@
+toolboxImg = document.querySelectorAll('.toolbox');
+
+(function(){
+    toolboxImg[0].style.animation = "move1 1s infinite";
+    toolboxImg[1].style.animation = "move1 1.5s infinite";
+    toolboxImg[2].style.animation = "move1 2s infinite";
+    toolboxImg[3].style.animation = "move1 2s infinite";
+    toolboxImg[4].style.animation = "move1 1.5s infinite";
+    toolboxImg[5].style.animation = "move1 1s infinite";
+    toolboxImg[6].style.animation = "move1 .5s infinite";
+})();
