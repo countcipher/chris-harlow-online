82750b78eeb84a8502c0366a72fceed6daecc4f9
Count Cipher <chrisharlow366@yahoo.com>
2021-11-13 10:57:30 -0500

    finished contact me section, modal, and js for modal

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 const toolboxImg = document.querySelectorAll('.toolbox');
-
 const sections = document.querySelectorAll('section');
+const contact_modal_button = document.querySelector('#contact_modal_button');
+const contact_modal = document.querySelector("#contact_modal");
+const close_modal_button = document.querySelector('#close_modal_button');
 
+
+//Trigger for showing sections onscroll
 window.addEventListener('scroll', showSections);
 
+//Function for showing modal
+contact_modal_button.addEventListener('click', () => {
+    contact_modal.classList.add('reveal');
+});
+
+//Function for closing modal
+close_modal_button.addEventListener('click', () => {
+    contact_modal.classList.remove('reveal');
+});
+
+//Anonymous function controlling bouncing logos in
+//Toolbox section
 (function(){
     toolboxImg[0].style.animation = "move1 1s infinite";
     toolboxImg[1].style.animation = "move1 1.5s infinite";
@@ -14,6 +30,7 @@ window.addEventListener('scroll', showSections);
     toolboxImg[6].style.animation = "move1 .5s infinite";
 })();
 
+//Showing sections onscroll function
 function showSections(){
     const triggerBottom = window.innerHeight - 150;
 
